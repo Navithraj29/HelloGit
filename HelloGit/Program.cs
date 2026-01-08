5805778f77cbdb7f7aa049ca4353d4da9aeda9d6
Navith Pandra <Navith.Pandra@meridianitinc.com>
2026-01-08 16:43:21 +0530

    Initial commit: HelloGit console app

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello Git!");
+Console.WriteLine("This is my first clean GitHub project 🚀");
+
